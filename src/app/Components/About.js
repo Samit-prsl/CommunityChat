c8eddace0f01ce1ab6953042e7b215c237fcd334
Samit <Samit.prsl@gmail.com>
2023-08-21 22:54:59 +0530

    Build error fixed

--- src/app/Components/About.js
+++ src/app/Components/About.js
@@ -11,7 +11,7 @@ export default function About() {
       <div className=' flex flex-col justify-center gap-5 p-5 lg:w-1/2 w-full bg-yellow-50 mt-3 lg:mt-0'>
         <img src="https://images.unsplash.com/photo-1588196749597-9ff075ee6b5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVldHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" className=' h-64 w-full' />
         <h1 className=' lg:text-xl text-lg'>
-        Video calls with fellow peers can significantly increase productivity by fostering positive responses, enhancing productive skills, and promoting understanding. Engaging in meaningful meetings shows that you value and respect everyone's thoughts and opinions, creating a sense of appreciation. Through open and friendly discussions, you can build confidence and connections, leading to productive mindset</h1>
+        Video calls with fellow peers can significantly increase productivity by fostering positive responses, enhancing productive skills, and promoting understanding. Engaging in meaningful meetings shows that you value and respect everyones thoughts and opinions, creating a sense of appreciation. Through open and friendly discussions, you can build confidence and connections, leading to productive mindset</h1>
       </div>
       <div className=' flex flex-col justify-center gap-5 p-5 lg:w-1/2 w-full bg-yellow-50 mt-3 lg:mt-0'>
         <img src="https://images.unsplash.com/photo-1511632765486-a01980e01a18?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNvbW11bml0eXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" className=' h-64 w-full' /> 
